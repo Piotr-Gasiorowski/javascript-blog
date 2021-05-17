3c5bbff8757f464fbb941afc6989f506ac663482
Piotr Gąsiorowski <p.gasiorowski@wp.pl>
2021-05-17 17:02:50 +0200

    Add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -4,17 +4,24 @@
 });
 */
 const titleClickHandler = function(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
+  console.log('clickedElement (with plus): ' + clickedElement);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.post.active');
 
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
-  /* add class 'active' to the clicked link */
-
-  /* remove class 'active' from all articles */
 
   /* get 'href' attribute from the clicked link */
 
@@ -28,4 +35,3 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
-console.log('Click');
