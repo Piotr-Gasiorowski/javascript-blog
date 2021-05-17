d4938b5c4c8df12a7bf8f2e159b6746655eba062
Piotr Gąsiorowski <p.gasiorowski@wp.pl>
2021-05-17 17:33:28 +0200

    Complete task

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 });
 */
 const titleClickHandler = function(event){
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
   console.log('clickedElement (with plus): ' + clickedElement);
@@ -14,7 +15,8 @@ const titleClickHandler = function(event){
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
+  clickedElement.classList.add('active');
 
   /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.post.active');
@@ -23,12 +25,13 @@ const titleClickHandler = function(event){
     activeArticle.classList.remove('active');
   }
 
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
 
-  /* find the correct article using the selector (value of 'href' attribute) */
-
-  /* add class 'active' to the correct article */
-}
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+const targetArticle = document.querySelector(articleSelector);
+  /* [DONE] add class 'active' to the correct article */
+targetArticle.classList.add('active');
 
 const links = document.querySelectorAll('.titles a');
 
