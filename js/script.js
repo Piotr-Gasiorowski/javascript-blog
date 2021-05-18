9a189337f2fd91a7e07e5354a07c2fe874206043
Piotr Gąsiorowski <p.gasiorowski@wp.pl>
2021-05-18 11:43:49 +0200

    End of task

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 const titleClickHandler = function(event){
   event.preventDefault();
   const clickedElement = this;
-  console.log('Link was clicked!');
+/*  console.log('Link was clicked!');
 
+  const links = document.querySelectorAll('.titles a');
+
+  for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+}
+*/
   /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -11,9 +17,9 @@ const titleClickHandler = function(event){
   }
   /* [DONE] add class 'active' to the clicked link */
   clickedElement.classList.add('active');
-
+  console.log('clikedElement:', clickedElement);
   /* [DONE] remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.post.active');
+  const activeArticles = document.querySelectorAll('.posts article.active');
 
   for(let activeArticle of activeArticles) {
     activeArticle.classList.remove('active');
@@ -23,13 +29,8 @@ const titleClickHandler = function(event){
   const articleSelector = clickedElement.getAttribute('href');
 
   /* [DONE] find the correct article using the selector (value of 'href' attribute) */
-const targetArticle = document.querySelector(articleSelector);
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
   /* [DONE] add class 'active' to the correct article */
-targetArticle.classList.add('active');
-
-const links = document.querySelectorAll('.titles a');
-
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
-}
-}
+  targetArticle.classList.add('active');
+};
